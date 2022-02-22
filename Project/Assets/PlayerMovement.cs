96d8eabcfb8619d5a8daae2e9f6c385c55f07b87
sinsus13 <sinsus13@gmail.com>
2022-02-22 21:54:01 +0330

    readded player added player movement added floats for player rotation

--- Project/Assets/PlayerMovement.cs
+++ Project/Assets/PlayerMovement.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float Force = 500f;
+    public float Jump = 500f;
+    public float Rotate = 5f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public bool Floored = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey("a") || (Input.GetKey("left")))
+        {
+
+            rb.AddRelativeForce(0, 0, -Force * Time.deltaTime, ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKey("d") || (Input.GetKey("right")))
+        {
+
+            rb.AddRelativeForce(0, 0, Force * Time.deltaTime, ForceMode.VelocityChange);
+
+        }
+
+        rb.transform.Rotate(0, -Rotate, 0, Space.Self);
+        rb.transform.Rotate(0, Rotate, 0, Space.Self);
+
+        if (Input.GetKey("w") || (Input.GetKey("up")))
+        {
+
+            rb.AddRelativeForce(-Force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKey("s") || (Input.GetKey("down")))
+        {
+
+            rb.AddRelativeForce(Force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKeyDown("space") && Floored)
+        {
+
+
+            rb.AddRelativeForce(new Vector3(0, Jump, 0), ForceMode.Impulse);
+            Floored = false;
+
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKeyDown("space"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+        }
+
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Ground")
+        {
+
+            Floored = true;
+
+        }
+
+    }
+
 }
